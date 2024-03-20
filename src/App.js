51c80eb455cb1ae6612a1c3cf24326cf21b77982
OmniEskay <skamtolo10@gmail.com>
2024-03-20 13:39:09 +0300

    create sign in, sign up and forgot password pages and oauth components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
+import SignIn from  './pages/SignIn'
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
